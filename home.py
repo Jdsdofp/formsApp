277bcd08fd9415061bb6cba17c64041c0a834ede
Jds Dofp <jdsdofp@gmail.com>
2023-10-11 10:13:22 -0300

    la gata rata

--- home.py
+++ home.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 # Cabeçalho personalizado
 st.set_page_config(
-    page_icon="img\Logo_CoraçãoDrogaria_Globo.ico",
+    page_icon="Logo_CoraçãoDrogaria_Globo.ico",
     page_title="Forms SCs",
     layout="wide"
 )
 
 st.title("Formulario de abertura SCs", anchor=False)
-st.sidebar.image("img\Logo_CoraçãoDrogaria_Globo.png", caption="Forms App - Controle de SCs")
+st.sidebar.image("Logo_CoraçãoDrogaria_Globo.png", caption="Forms App - Controle de SCs")
 
 col1, col2 = st.columns(2)
 col1.metric(label="Aberto",value="20", help="Numero de Solicitações que foram abertas hoje", delta="+1")
