495347fa3720378e01c400f89d26c8d0511d7f77
Jds Dofp <jdsdofp@gmail.com>
2023-10-30 10:21:00 -0300

    Finals2

--- ps.py
+++
@@ -1,9 +0,0 @@
-
-with open('requirements.txt', 'r') as f:
-    packages = f.read().splitlines()
-
-packages = [package.split("==")[0] for package in packages]
-
-with open('requirements.txt', 'w') as f:
-    for package in packages:
-        f.write(package + '\n')
