801638d1c4fb7ba67ed59a3b39b91a00e3eda579
Jds Dofp <jdsdofp@gmail.com>
2024-02-27 13:21:14 -0300

    integração com email

--- models.py
+++ models.py
@@ -7,16 +7,3 @@ mydb = client["mydatabase"]
 col_solicitacao = mydb["registros"]
 col_filiais = mydb["filiais"]
 col_usuario = mydb["usuarios"]
-
-
-# Critério de busca
-filtro = {"solicitante": "Weslley"}
-
-# Novo valor a ser definido
-novo_valor = {"$set": {"solicitante": "Weslley Passos"}}
-
-# Atualizando os documentos que correspondem ao critério
-resultado = col_solicitacao.update_many(filtro, novo_valor)
-
-# Exibindo o número de documentos atualizados
-print(f"{resultado.modified_count} documentos foram atualizados.")
